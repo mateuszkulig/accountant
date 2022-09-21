9976db2dfa27cb5342557220812d763040362922
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-09-22 01:05:54 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='accountant',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/mateuszkulig/accountant',
+    license='GNU General Public License v3.0',
+    author='Mateusz Kulig',
+    author_email='mateusz.kulig02@gmail.com',
+    description='Tool written in python to make bot and web accounts easy to create.'
+)
