9cfe9acb011e52e79d5c71ca2292098fd18c886a
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-10-31 00:56:32 +0100

    moved 10minutemail control from acc to standalone module in accounts; removed some comments

--- acc.py
+++ acc.py
@@ -119,6 +119,7 @@ class Browser(webdriver.Chrome):
                 print(f"element (\"{xpath}\") not interactable; error")
                 self.wait_n_click(xpath, timeout)
 
+    # todo: replaced with safe_interact, check for refernces and delete
     def safe_click(self, xpath, timeout=15):
         """call wait_n_click while watching for errors that hang selenium process"""
         try:
@@ -137,54 +138,6 @@ class Browser(webdriver.Chrome):
         el = self.wait_for_element(xpath, timeout)
         if el is not None:
             self.execute_script("a = document.evaluate('%s', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; a.click();" % xpath)
-
-class Mail(Browser):
-    """10minutemail.net control"""
-    def __init__(self):
-        super(Mail, self).__init__(adp=True)
-        self.mail = ""
-        self.get("https://10minutemail.net/")
-        self.get_mail()
-
-    def __str__(self):
-        return "mail browser"
-
-    def get_mail(self):
-        """get fresh mail adress"""
-        el_mailinput = self.wait_for_element('//*[@id="fe_text"]')
-        self.mail = el_mailinput.get_attribute("value")
-        print(f"got email adress: \"{self.mail}\"")
-
-    def get_messages(self):
-        """get and return all messages recieved"""
-        mailtable = self.wait_for_element('//*[@id="maillist"]/tbody')
-        items = len(mailtable.find_elements(By.XPATH, "./tr")) - 1
-        print(f"found {items} messages")
-        topics = []
-        for i in range(items):
-            singlemailtable = mailtable.find_elements(By.XPATH, "./tr")[i + 1]
-            sender = singlemailtable.find_element(By.XPATH, "./td[1]")
-            topic = singlemailtable.find_element(By.XPATH, "./td[2]")
-            date = singlemailtable.find_element(By.XPATH, "./td[3]")
-            topics.append(topic.text)
-            print(f"\t{i}\t-\tfrom: \"{sender.text}\"\ttopic: \"{topic.text}\"\tdate: \"{date.text}\"")
-        return topics
-
-    def confirm_mail(self, mailtopic:str):
-        """opens and reads message recieved + clicks the link"""
-        idx = -1
-        while idx == -1:
-            msg_topics = self.get_messages()
-            for t in msg_topics:
-                if t == mailtopic:
-                    idx = msg_topics.index(mailtopic)
-            time.sleep(1)
-        self.wait_n_click(f'//*[@id="maillist"]/tbody/tr[{idx+2}]', timeout=120)
-        self.wait_for_element('//h1[text()="Witamy w świecie bananków!"]')
-        passwd_line = self.wait_for_element('//*[@id="tab1"]/div[1]/div/div[2]/div[2]/h2[1]').text  # todo: shitty xpath
-        temp_passwd = passwd_line.split(" ")[-1]
-        self.wait_n_click('//a[text()="POTWIERDŹ EMAIL"]')
-        return temp_passwd
 
 
 if __name__ == "__main__":
---
+++ accounts/10minutemail.py
@@ -0,0 +1,51 @@
+from acc import Browser
+import time
+from selenium.webdriver.common.by import By
+
+class Mail(Browser):
+    """10minutemail.net control"""
+    def __init__(self):
+        super(Mail, self).__init__(adp=True)
+        self.mail = ""
+        self.get("https://10minutemail.net/")
+        self.get_mail()
+
+    def __str__(self):
+        return "mail browser"
+
+    def get_mail(self):
+        """get fresh mail adress"""
+        el_mailinput = self.wait_for_element('//*[@id="fe_text"]')
+        self.mail = el_mailinput.get_attribute("value")
+        print(f"got email adress: \"{self.mail}\"")
+
+    def get_messages(self):
+        """get and return all messages recieved"""
+        mailtable = self.wait_for_element('//*[@id="maillist"]/tbody')
+        items = len(mailtable.find_elements(By.XPATH, "./tr")) - 1
+        print(f"found {items} messages")
+        topics = []
+        for i in range(items):
+            singlemailtable = mailtable.find_elements(By.XPATH, "./tr")[i + 1]
+            sender = singlemailtable.find_element(By.XPATH, "./td[1]")
+            topic = singlemailtable.find_element(By.XPATH, "./td[2]")
+            date = singlemailtable.find_element(By.XPATH, "./td[3]")
+            topics.append(topic.text)
+            print(f"\t{i}\t-\tfrom: \"{sender.text}\"\ttopic: \"{topic.text}\"\tdate: \"{date.text}\"")
+        return topics
+
+    def confirm_mail(self, mailtopic:str):
+        """opens and reads message recieved + clicks the link"""
+        idx = -1
+        while idx == -1:
+            msg_topics = self.get_messages()
+            for t in msg_topics:
+                if t == mailtopic:
+                    idx = msg_topics.index(mailtopic)
+            time.sleep(1)
+        self.wait_n_click(f'//*[@id="maillist"]/tbody/tr[{idx+2}]', timeout=120)
+        self.wait_for_element('//h1[text()="Witamy w świecie bananków!"]')
+        passwd_line = self.wait_for_element('//*[@id="tab1"]/div[1]/div/div[2]/div[2]/h2[1]').text  # todo: shitty xpath
+        temp_passwd = passwd_line.split(" ")[-1]
+        self.wait_n_click('//a[text()="POTWIERDŹ EMAIL"]')
+        return temp_passwd
