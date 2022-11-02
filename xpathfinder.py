b1c3c20893d2af10fb8911e18b83ea9e898c2212
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-11-02 23:34:29 +0100

    added combine_xpath function; refactored main code into name=main condition

--- xpathfinder.py
+++ xpathfinder.py
@@ -1,6 +1,14 @@
+"""
+xpathfinder.py
+by Mateusz Kulig
+Script to generate xpaths from clicks on elements in selenium webdriver browser.
+"""
+
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
 from pprint import pprint
 
 URL_GAZETA = "https://konto.gazeta.pl/konto/rejestracja.do"
@@ -20,20 +28,29 @@ JS_GET_ARGS_FROM_ELEMENT = """
         return items;
     """
 
-driver = webdriver.Chrome()
-driver.maximize_window()
+def combine_xpath(tag_name, attrs:dict) -> str:
+    """combine xpath from element tag and attributes"""
+    return f"//{tag_name}".join(f"[@{k}=\"{attrs[k]}\"]" if k != "class" else f"[@{k}=\"{''.join(val for val in attrs[k])}\"]" for k in attrs)
 
-driver.get(URL_GAZETA)
-driver.execute_script(JS_GET_CLICKED_ELEMENT)
 
-clicked_element:WebElement
-while True:
-    leave = input("enter to continue; type anything to exit\n")
-    if leave:
-        break
-    clicked_element = driver.execute_script("return document.a")
-    html = clicked_element.get_attribute('outerHTML')
-    attributes = BeautifulSoup(html, 'html.parser').find().attrs
-    pprint(attributes)
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.maximize_window()
 
-driver.quit()
+    driver.get(URL_GAZETA)
+    driver.execute_script(JS_GET_CLICKED_ELEMENT)
+
+    clicked_element: WebElement
+    while True:
+        leave = input("enter to continue; type anything to exit\n")
+        if leave:
+            break
+        clicked_element = driver.execute_script("return document.a")
+        html = clicked_element.get_attribute('outerHTML')
+        parsed_element = BeautifulSoup(html, 'html.parser').find()
+        tag = parsed_element.name
+        attributes = parsed_element.attrs
+        xp = combine_xpath(tag, attributes)
+        print(xp)
+
+    driver.quit()
