60795e9b836feab9e0408e0ae4c33447ee2fd10a
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-11-09 21:37:29 +0100

    add RecievedEmail class; change headers and cookies to static

--- apis/gazetamail.py
+++ apis/gazetamail.py
@@ -2,36 +2,29 @@ import requests
 import json
 
 
-class RecievedEmail(object):
-    """one mail object"""
-    def __init__(self, mess_id:int, sender:str, date:str, subject:str):
-        self.id = mess_id
-        self.sender = sender
-        self.date = date
-        self.subject = subject
-
-
 class GazetaMailApi(object):
     """gazeta.pl mail api"""
-    def __init__(self, cookies:dict):
-        self.cookies = cookies
-        self.headers = {
-            'authority': 'poczta.gazeta.pl',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        }
+    headers = {
+        'authority': 'poczta.gazeta.pl',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    }
+    cookies = {}
 
-    def get_messages(self) -> list[RecievedEmail]:
+    def __init__(self, cookies:dict):
+        self.__class__.cookies = cookies
+
+    def get_messages(self) -> list:
         """get and parse all of recieved emails"""
         messages = []
         response = requests.get('https://poczta.gazeta.pl/webmailapi/mail/', cookies=self.cookies, headers=self.headers)
@@ -44,6 +37,19 @@ class GazetaMailApi(object):
             messages.append(RecievedEmail(mess_id, sender, date, subject))
         return messages
 
+
+class RecievedEmail(object):
+    """one mail object"""
+    def __init__(self, mess_id:int, sender:str, date:str, subject:str):
+        self.id = mess_id
+        self.sender = sender
+        self.date = date
+        self.subject = subject
+        self.content = self.get_message_html()
+
+    def get_message_html(self):
+        """get message html content"""
+        return requests.get(f'https://poczta.gazeta.pl/webmailapi/mail/{self.id}', cookies=GazetaMailApi.cookies, headers=GazetaMailApi.headers).text
 
 
 
